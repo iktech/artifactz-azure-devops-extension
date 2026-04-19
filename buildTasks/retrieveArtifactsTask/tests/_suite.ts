0dfebbcba38da85e033064256031051ba9b25648
Igor Kolomiyets <ikolomiyets@gmail.com>
2026-04-19 17:45:06 +0100

    Fix unit tests

--- buildTasks/retrieveArtifactsTask/tests/_suite.ts
+++ buildTasks/retrieveArtifactsTask/tests/_suite.ts
@@ -21,7 +21,7 @@ describe('Retrieve Artifacts task tests', function () {
         assert.strictEqual(tr.warningIssues.length, 0, "should have no warnings");
         assert.strictEqual(tr.errorIssues.length, 0, "should have no errors");
         assert.strictEqual(tr.stdout.indexOf('Successfully retrieved artifacts from the server') >= 0, true, "should display success message");
-        assert.strictEqual(tr.stdout.indexOf('##vso[task.setvariable variable=artifacts;isOutput=true;issecret=false;]{"test-data":"1.0.1","gradle-plugin-test":"1.0.0-SNAPSHOT"}') >= 0, true, "should set variable");
+        assert.strictEqual(tr.stdout.indexOf('##vso[task.setvariable variable=artifacts;isOutput=true;issecret=false;]{"test-data":"1.0.0","gradle-plugin-test":"1.1.1-SNAPSHOT"}') >= 0, true, "should set variable");
     });
 
     it('should succeed with single artifact', async () => {
